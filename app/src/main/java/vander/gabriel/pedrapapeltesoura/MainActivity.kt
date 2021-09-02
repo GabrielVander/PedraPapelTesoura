756b4ab4725204372fb37d8dbde6b4353a114fa1
Gabriel Vander <gabsvander@hotmail.com>
2021-09-02 14:36:19 -0300

    Initial commit

---
+++ app/src/main/java/vander/gabriel/pedrapapeltesoura/MainActivity.kt
@@ -0,0 +1,38 @@
+package vander.gabriel.pedrapapeltesoura
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import vander.gabriel.pedrapapeltesoura.ui.theme.PedraPapelTesouraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PedraPapelTesouraTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PedraPapelTesouraTheme {
+        Greeting("Android")
+    }
+}
